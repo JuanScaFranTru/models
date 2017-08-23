cb1386548ebc0354f02caf092fb0519b0dadfc29
Francisco Trucco <franciscoctrucco@gmail.com>
2017-08-22 21:29:46 -0300

    Change rigdecv.py non-positive model to a positive one

--- models/linear/rigdecv.py
+++ models/linear/rigdecv.py
@@ -1,11 +1,11 @@
-from sklearn.linear_model import RidgeCV
+from models.positive_models import PositiveRidgeCV
 from models.utils import load_data, plot_prediction, print_stats
 
 
 if __name__ == '__main__':
     data_len, weeks, ts, xs = load_data()
 
-    linear_model = RidgeCV()
+    linear_model = PositiveRidgeCV()
     linear_model.fit(xs, ts)
 
     print_stats(xs, ts, linear_model)
