5c11bc02f07f2687755bcdb6f79775fe7d0ee7e7
Francisco Trucco <franciscoctrucco@gmail.com>
2017-08-23 17:45:32 -0300

    Finish run_model.sh. Ignore Rdata.

--- run_model.sh
+++ run_model.sh
@@ -1,6 +1,21 @@
 #! /bin/bash
-
 declare -a models=('SVR' 'RBF')
+
+
+# If the number of arguments is 0, show an error message and exit
+if [ $# -eq 0 ]
+then
+    echo -e "${bold}Falta el argumento del modelo";
+    echo -e "Modelos disponibles:"
+    for model in "${models[@]}"; do
+        echo -e "- $model"
+    done
+    echo -e "- ALL"
+
+    # Exit with error
+    exit 1;
+fi
+
 
 cd tuning/
 
@@ -14,7 +29,6 @@ else
     path="../models/$model"
     irace -p $path/parameters.txt --exec-dir $path/ > $path/results.results;
 fi
-
 
 # Trap ctrl-c and other exit signals and delete all temporary files
 trap TrapError 1 2 3 15;
