19e56da9ec144f86c940632f5b600fc688099dfe
Francisco Trucco <franciscoctrucco@gmail.com>
2017-08-24 16:31:40 -0300

    Add installer and environment to support anaconda. Delete requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# models-compare
+# Models
+
+## Install and uninstall
+### Install
+
+You must have anaconda installed in order to install the package and the environment.
+Download anaconda from [here](https://www.continuum.io/downloads).
+
+```
+./installer.sh install;
+```
+
+**Note**: This will create a new environment.
+
+### Uninstall
+To uninstall run:
+
+```
+./installer.sh uninstall;
+```
+
+## Running irace
+
+```
+./run_model.sh MODEL
+```
+
+to see which models are available:
+
+```
+./run_model.sh
+```
+
+to run all models:
+
+```
+./run_model.sh ALL
+```
+
+## [TODO] Creating results table
---
+++ installer.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+PACKAGE_NAME=models;
+ENV=environment.yml
+ENV_NAME=$(head -n 1 $ENV | sed 's|name: ||')
+
+function install_all {
+    # Install environment and package
+    conda env create -f $ENV;
+    source activate $ENV_NAME;
+    python setup.py install;
+}
+
+function uninstall_all {
+    source deactivate $ENV_NAME;
+    python setup.py install --record uninstall_files.txt > /dev/null;
+    cat uninstall_files.txt | xargs rm -rf;
+    rm -fr models.egg-info build dist uninstall_files.txt;
+    conda remove --name $ENV_NAME --all;
+}
+
+# MAIN ----------------------------------------------------------------------
+
+if [ "$1" = "uninstall" ]; then
+    uninstall_all;
+
+elif [ "$1" = "install" ]; then
+    install_all;
+
+else
+    echo -e "${bold}Missing argument:";
+    echo -e "Usage:"
+    echo -e "./installer.sh install|uninstall"
+fi
+
+# Trap ctrl-c and other exit signals
+trap TrapError 1 2 3 15;
+function TrapError() {
+    echo "Something went wrong...";
+    exit 1;
+}
